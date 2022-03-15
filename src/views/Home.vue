8e572f2e7d4fc548066e8928b29f21455423c48a
pxdev <drpxdev@gmail.com>
2022-03-16 01:08:33 +0300

    v1.3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,12 +29,12 @@
         <div class="landing-navigation ">
           <div class="container">
             <VueSlickCarousel class="slick-carousel landing-slick" v-bind="Settings">
-            <div v-for="(n, index) in 10" :key="index + '_group'" class="pd-x-10">
+            <div v-for="(sector, index) in sectors" :key="index + '_group'" class="pd-x-10">
                 <router-link to="/explore" class="home-nav-item">
                   <svg class="svg-ico" width="42.053" height="39.501" viewBox="0 0 42.022 38.989">
-                    <use class="svg-ico-use" xlink:href="icons/icon.symbol.svg#conference-ico"></use>
+                    <use class="svg-ico-use" :xlink:href="`icons/icon.symbol.svg#${sector.iconId}`"></use>
                   </svg>
-                  <span>البنية التحتية</span>
+                  <span>{{ sector.label }}</span>
                   <span class="explore-btn tx-12 d-flex align-items-center"><i class="ri-arrow-left-line"></i><span class="pd-x-5">استكشف</span></span>
                 </router-link>
             </div>
@@ -67,13 +67,195 @@ export default {
   components: {HomeMap, PageHeader, VideoBg, VueSlickCarousel},
   data() {
     return {
+      sectors: [
+        {
+          id: '01',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: ' السكان والخصائص السكانية ',
+          children: [
+            {
+              id: ' 01-01',
+              label: '  عدد الأسر ',
+              isActive: true,
+            },
+            {
+              id: '01-02',
+              label: ' المعدل السنوي  الأسر الجديدة ',
+              isActive: false,
+            }
+          ],
+        },
+        {
+          id: ' 02',
+          iconId: "conference-ico",
+          isActive: true,
+          isOpened: true,
+          label: 'المياه والبيئة',
+          children: [
+            {
+              id: ' 02-01 ',
+              label: '  تركيز ثاني أكسيد الكبريت  ',
+              isActive: true,
+            },
+            {
+              id: ' 02-02 ',
+              label: '  عدد محطات الرصد والمراقبة  ',
+              isActive: false,
+            },
+            {
+              id: ' 02-03 ',
+              label: '  نسبة زمن الاستجابة  دقائق فأقل  ',
+              isActive: false,
+            },
+
+            {
+              id: '  02-04 ',
+              label: '   زمن الاستجابة  دقائق فأقل  ',
+              isActive: false,
+            },
+          ],
+        },
+        {
+          id: ' 03 ',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: '  إدارة الكوارث  ',
+          children: [
+            {
+              id: ' 03-01 ',
+              label: '  نسبة زمن الاستجابة للحوادث خلال سبع دقائق فأقل  ',
+              isActive: false,
+            },
+            {
+              id: '03-02',
+              label: ' المعدل السنوي الأسر الجديدة ',
+              isActive: false,
+            }
+          ],
+        },
+        {
+          id: ' 04',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: '  الشؤون الإسلامية والحج والعمرة ',
+          children: []
+        },
+        {
+          id: '05',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'الأمن والسلامة',
+          children: []
+        },
+        {
+          id: '06',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'الايواء والمساكن',
+          children: []
+        },
+        {
+          id: '07',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'البنية التحتية',
+          children: []
+        },
+        {
+          id: '08',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'الطاقة',
+          children: []
+        },
+        {
+          id: '09',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'الحوكمة والتشريعات',
+          children: []
+        },
+        {
+          id: '10',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'الثقافة والإعلام',
+          children: []
+        },
+        {
+          id: '11',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'الصحة',
+          children: []
+        },
+        {
+          id: '12',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'النمو الاقتصادي',
+          children: []
+        },
+        {
+          id: '13',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'التعليم',
+          children: []
+        },
+        {
+          id: '14',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'العمل والحماية الاجتماعية',
+          children: []
+        },
+        {
+          id: '15',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'النسيج العمراني',
+          children: []
+        },
+        {
+          id: '16',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'تمويل البلديات',
+          children: []
+        },
+        {
+          id: '17',
+          iconId: "conference-ico",
+          isActive: false,
+          isOpened: false,
+          label: 'السياحة والترفيه والرياضة',
+          children: []
+        },
+      ],
       Settings: {
         "dots": false,
         "infinite": false,
         "speed": 500,
         "slidesToShow": 7,
-        "slidesToScroll": 1,
-        "touchThreshold": 1,
+        "slidesToScroll": 7,
+        "touchThreshold": 7,
         "adaptiveHeight": true,
 
         "responsive": [
