3a635946cb086a493aa97debdf4ec8a1d56814f3
pxdev <drpxdev@gmail.com>
2022-03-15 21:18:32 +0300

    :rocket: change the side nav toggle function

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -1,7 +1,6 @@
 <template>
   <aside class="side-bar">
 
-
     <nav class="side-nav card shadow rounded">
 
       <div class="quick-search mg-y-20">
@@ -9,16 +8,14 @@
         <treeselect placeholder="بحث عن المؤشرات" v-model="value" :options="sectors"/>
       </div>
       <ul>
-        <li :class="{'active' : sector.isOpened==true }"  v-for="(sector, index) in sectors" :key="index + '_sector'">
-          <a href="#" class="side-nav-link" @click.prevent="toggleNav(sector)" >
+        <li :class="{'active' : activeSector == index }" v-for="(sector, index) in sectors" :key="index + '_sector'">
+          <a href="#" class="side-nav-link" @click.prevent="toggleNav(index)" >
             <svg class="svg-ico" width="42.053" height="39.501" viewBox="0 0 42.022 38.989">
               <use class="svg-ico-use" :xlink:href="`icons/icon.symbol.svg#${sector.iconId}`"></use>
             </svg>
             <span class="pd-x-20">{{ sector.label }}</span></a>
-
-          <ul v-if="sector.children.length && sector.isOpened == true  " >
-            <li :class="{'active' : indicator.isActive==true }" v-for="(indicator, index) in sector.children"
-                :key="index + '_indicator'">
+          <ul v-if="sector.children.length && activeSector == index  " >
+            <li :class="{'active' : indicator.isActive==true }" v-for="(indicator, index) in sector.children" :key="index + '_indicator'">
               <a href="#">{{ indicator.label }}</a>
             </li>
           </ul>
@@ -39,9 +36,8 @@ export default {
   components: {Treeselect},
   data() {
     return {
-
+      activeSector : 0,
       value: null,
-
       sectors: [
         {
           id: 'السكان والخصائص السكانية',
@@ -132,8 +128,8 @@ export default {
   }
   ,
   methods:{
-    toggleNav(sector){
-      sector.isOpened = ! sector.isOpened
+    toggleNav(index){
+      this.activeSector = index
     }
   }
 }
